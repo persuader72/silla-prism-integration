e7e36c47af6141abe89264e1d847373502acf945
Stefano Pagnottelli <stefano.pagnottelli@gmail.com>
2024-06-14 15:35:25 +0200

    Removed unused manager code

--- custom_components/silla_prism/__init__.py
+++ custom_components/silla_prism/__init__.py
@@ -12,7 +12,6 @@ from homeassistant.helpers import config_validation as cv
 from .const import CONF_TOPIC, DOMAIN
 from .domain_data import DomainData
 from .entry_data import RuntimeEntryData
-from .manager import PrismManager
 
 _LOGGER = logging.getLogger(__name__)
 CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
@@ -26,8 +25,5 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     domain_data = DomainData.get(hass)
     entry_data = RuntimeEntryData(topic=_topic)
     domain_data.set_entry_data(entry, entry_data)
-    manager = PrismManager(hass=hass, domain_data=domain_data, entry_data=entry_data)
-    await manager.async_start()
-
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
